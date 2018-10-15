b0392395834e1e2b9208f1491e03706e6fa6bb1b
Lyn <lyn1990828@gmail.com>
2018-10-15 14:43:30 +0800

    init eureka

---
+++ src/main/java/com/i2bgod/springcloud/SpringCloudApplication.java
@@ -0,0 +1,14 @@
+package com.i2bgod.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApplication.class, args);
+    }
+}
