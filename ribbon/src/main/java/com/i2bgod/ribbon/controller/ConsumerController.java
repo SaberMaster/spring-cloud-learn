2683168800dda7bb36d0869b998f20d1fb4d8f55
Lyn <lyn1990828@gmail.com>
2018-10-15 16:55:16 +0800

    multi service and ribbon

---
+++ ribbon/src/main/java/com/i2bgod/ribbon/controller/ConsumerController.java
@@ -0,0 +1,24 @@
+package com.i2bgod.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @auther: Lyn
+ * @data: 2018/10/15
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+}
